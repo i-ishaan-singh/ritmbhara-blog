3a821d5ba1abdd7e76a6e37f96c7b2df4e94c878
ishaananuraag <ishaananuraag@gmail.com>
2024-01-26 15:59:10 +0530

    Removing comments to test deployment

--- rt-server/routes/data.js
+++ rt-server/routes/data.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/get-server-time', function(req, res, next) {
     res.send(new Date().toLocaleString())
 });
